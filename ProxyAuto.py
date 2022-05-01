9bf293347b2bf4a60f977a183ef89007625833ea
nelsongp1988 <56329369+nelsongp1988@users.noreply.github.com>
2022-05-01 09:49:07 -0400

    Update ProxyAuto.py

--- ProxyAuto.py
+++ ProxyAuto.py
@@ -11,12 +11,12 @@ def Search_ping():
     print("Buscando proxy...")
     for port in range(1000,9999): # El rango del puerto
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-        result = sock.connect_ex(('152.206.85.87',port)) # IP o dirección,puerto o poner la variable port para escanear los puertos
+        result = sock.connect_ex(('181.225.253.188',port)) # IP o dirección,puerto o poner la variable port para escanear los puertos
 
         if result == 0: # Si el puerto está activo
             print ("Puerto abierto!")
             print (f"Puerto: {port}")  
-            proxy = f'152.206.85.87:{port}'
+            proxy = f'181.225.253.188:{port}'
             proxy_new = S5Crypto.encrypt(f'{proxy}')
             break
         else: # Si hay error hal escanear un puerto
